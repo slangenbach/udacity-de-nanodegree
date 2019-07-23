4b2b89bee72440c99d36cf8b3be2556c0eb54178
Stefan Langenbach <stefan.buenten@gmail.com>
2019-07-23 18:56:41 +0200

    Add additional parameters for data quality operator

--- airflow/plugins/operators/data_quality.py
+++ airflow/plugins/operators/data_quality.py
@@ -15,17 +15,27 @@ class DataQualityOperator(BaseOperator):
     def __init__(self,
                  redshift_conn_id: str,
                  check_tables: list,
+                 check_sql: str,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.check_tables = check_tables
+        self.check_sql = check_sql
 
     def execute(self, context):
 
         # run quality checks
         for table in self.check_tables:
-            logging.info(f"Running data quality check for table: {table}")
 
-            # check if data has been loaded into dimensions tables
-            CheckOperator(f"SELECT COUNT(*) FROM {table}", conn_id=self.redshift_conn_id)
+            # run built-in quality checks
+            if self.check_sql == "has_rows":
+                logging.info(f"Running data quality check for table: {table}")
+
+                # check if data has been loaded into dimensions tables
+                CheckOperator(f"SELECT COUNT(*) FROM {table}", conn_id=self.redshift_conn_id)
+
+            # run custom quality check
+            else:
+                logging.info("Running data quality check")
+                CheckOperator(self.check_sql, conn_id=self.redshift_conn_id)
