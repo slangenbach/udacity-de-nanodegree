4aa29add6bcc22c585804d6c642d9c8d3d97d5f7
Stefan Langenbach <stefan.buenten@gmail.com>
2019-09-11 13:33:27 +0200

    Add extraction of user_dates

--- capstone/sql/time_insert.sql
+++ capstone/sql/time_insert.sql
@@ -1,10 +1,21 @@
 INSERT INTO time ("date", "second", "minute", "hour", week, "month", "year", weekday)
-SELECT tweet_date,
-       extract(second from "date"),
-       extract(minute from "date"),
-       extract(minute from "date"),
-       extract(week from "date"),
-       extract(month from "date"),
-       extract(year from "date"),
-       extract(dayofweek from "date")
+SELECT DISTINCT tweet_date,
+       extract(second from tweet_date),
+       extract(minute from tweet_date),
+       extract(hour from tweet_date),
+       extract(week from tweet_date),
+       extract(month from tweet_date),
+       extract(year from tweet_date),
+       extract(dayofweek from tweet_date)
+FROM staging_tweets
+UNION
+SELECT DISTINCT user_date,
+        extract(second from user_date),
+        extract(minute from user_date),
+        extract(hour from user_date),
+        extract(week from user_date),
+        extract(month from user_date),
+        extract(year from user_date),
+        extract(dayofweek from user_date)
 FROM staging_tweets;
+
